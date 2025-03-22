a5b9a41fdfe05646632cc3670e432b6469e5270d
Cintriano <Vdanilofernandes9077@gmail.com>
2025-03-21 21:36:25 -0300

    Atividade blog finalizada

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -2,39 +2,68 @@ import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
 
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
 
+import api from '../../../../lib/api';
+import { useState, useEffect } from "react";
+
+interface User {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  login: string;
+  followers: number;
+  location: string;
+};
 
 export function Summary() {
 
-  // "https://api.github.com/users", "/lucaspedronet"
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+  async function getUser() {
+    try {
+      const response = await api.get('users/DaniloFernanades');
+      const data = response.data;
+
+      setUser(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getUser()
+}, []);
+
+  
+
+  // "https://api.github.com/users", "/DaniloFernanades"
   // "https://api.github.com/search"
   // "https://api.github.com/repos/lucaspedronet/TudoLista/issues"
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={user?.avatar_url} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{user?.name}</h1>
+          <a href="https://github.com/DaniloFernanades" target="_blank">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{user?.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{user?.login}</span>
           </div>
 
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{user?.location}</span>
           </div>
 
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{user?.followers}</span>
           </div>
         </SummaryAnchors>
       </section>
