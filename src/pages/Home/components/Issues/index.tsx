668a0893fd769cdde217d189ce7731e9dba38177
Cintriano <Vdanilofernandes9077@gmail.com>
2025-03-31 17:17:12 -0300

    preparando os dados da api

--- src/pages/Home/components/Issues/index.tsx
+++ src/pages/Home/components/Issues/index.tsx
@@ -1,12 +1,38 @@
 // import { dateFormatter } from "../../../../utils/formatter";
 import { IssuesContainer, StyledNavLink } from "./styles";
+import { useState, useEffect } from "react";
+import api from '../../../../lib/api';
+
+interface Issue {
+  id: number;
+  title: string;
+  body: string;
+  created_at: string;
+};
 
 export function Issues() {
+ 
+    const [Issue, setIssue] = useState<Issue | null>(null);
+  
+    useEffect(() => {
+    async function getIssue() {
+      try {
+        const response = await api.get('users/DaniloFernanades');
+        const data = response.data;
+  
+        setIssue(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getIssue()
+  }, []);
+  
   return (
     <IssuesContainer>
       <StyledNavLink to="/issue">
         <div>
-          <h2>Exemplo de issue</h2>
+          <h2>{Issue?.title}</h2>
           <span>20/03/2025</span>
         </div>
         <p>body</p>
